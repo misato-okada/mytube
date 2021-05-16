d421e46ff14e7bade36eb05241c7dd9d0bc23192
misato-okada <bamoja3310@gmail.com>
2021-05-16 23:01:59 +0900

    共通関数と共通関数で使用する定数の作成

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/functions.php';
+$dbh = connectDb();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/_head.html' ?>
+
+<body>
+    <div class="wrapper">
+        <h1 class="title"><a href="index.php">MyTube</a></h1>
+        <a href="new.php" class="btn new-btn">NEW</a>
+        <section class="movie-list">
+            <div class="movie">
+                <p class="movie-title"><a href=""><!-- タイトルを出力 --></a></p>
+                <div class="youtube">
+                    <iframe src="https://www.youtube.com/embed/<!-- ビデオIDを出力 -->" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
